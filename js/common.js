0b795f38a5800eab70753401a00df1d68f7469bc
John24601 <a1031672042@qq.com>
2020-08-20 16:26:59 +0800

    13-合并冲突测试5

--- js/common.js
+++ js/common.js
@@ -2,5 +2,8 @@ function getAA() {
     console.log("我是原始分支的函数AA");
 }
 function getCC() {
-    console.log("我是原始分支的函数C");
+    console.log("我是原始分支的函数CC");
+}
+function asd() {
+    console.log("我是原始分支的函数asd");
 }
